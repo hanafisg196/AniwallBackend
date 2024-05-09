45bc559a907156418573bf63aba5c32f3a216784
hanafisg196 <123381210+hanafisg196@users.noreply.github.com>
2024-05-09 11:39:23 +0700

    Add api for wallpapers by category

--- app/Http/Controllers/Api/CategoryApiController.php
+++ app/Http/Controllers/Api/CategoryApiController.php
@@ -6,21 +6,54 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
 use App\Models\Slide;
+use App\Models\Wallpaper;
 use Illuminate\Http\Request;
 
 class CategoryApiController extends Controller
 {
-    public function categories(Request $request):CategoryCollection
+    public function categories(Request $request)
     {
-       $page = $request->input('page', 1);
-       $size = $request->input('size',10);
-
-       $categories = Category::latest();
-       $categories = $categories->paginate(perPage: $size, page:$page);
-       return new CategoryCollection($categories);
+        $page = intval($request->query('page', 1));
+        $perPage = intval($request->query('perPage', 5));
+    
+        $categories = Category::latest()
+                    ->paginate($perPage, ['*'], 'page', $page);
+    
+        return response()->json([
+            'data' => $categories->items(),
+            [
+                'total' => $categories->total(),
+                'per_page' => $categories->perPage(),
+                'current_page' => $categories->currentPage(),
+                'last_page' => $categories->lastPage(),
+                'from' => $categories->firstItem(),
+                'to' => $categories->lastItem()
+            ]
+        ]);
     }
 
-   
+    public function wallpapersByCat(Request $request, int $id) {
+    {
+        $page = intval($request->query('page', 1));
+        $perPage = intval($request->query('perPage', 5));
+    
+        $categories = Category::where('id', $id)
+                     ->with('wallpapers')
+                     ->paginate($perPage, ['*'], 'page', $page);
 
+        return response()->json([
+            'data' => $categories->items(),
+            [
+                'total' => $categories->total(),
+                'per_page' => $categories->perPage(),
+                'current_page' => $categories->currentPage(),
+                'last_page' => $categories->lastPage(),
+                'from' => $categories->firstItem(),
+                'to' => $categories->lastItem()
+            ]
+        ]);
+                                            
+    }
 
+    }
 }
