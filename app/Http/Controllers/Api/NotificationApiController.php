582a6ee8102d60c03e7644577fab0b0c0fd2157b
hanafisg196 <hanafisg196@gmail.com>
2024-11-20 23:50:49 +0700

    yadaaaaa

--- app/Http/Controllers/Api/NotificationApiController.php
+++ app/Http/Controllers/Api/NotificationApiController.php
@@ -13,18 +13,24 @@ class NotificationApiController extends Controller
         $credentials = (new Factory)->withServiceAccount(base_path('animwall-c2259-firebase-adminsdk-20yoo-a45f358d99.json'));
         $messaging = $credentials->createMessaging();
         $wallpaper = Wallpaper::find($wallpaperId);
+
+        $deepLink = "https://kyoani-publisher.xyz/wallpaper/" . $wallpaper->id;
+
         $message = CloudMessage::fromArray([
             "notification" => [
                 "title" => "New Wallpaper Uploaded",
                 "body" => "Check out the new wallpaper: " . $wallpaper->title
             ],
             "data" => [
+                "deepLink" => $deepLink,
                 "id" => $wallpaper->id
             ],
             "topic" => "global"
         ]);
+
         $messaging->send($message);
         return response()->json("send notification success");
     }
+
 
 }
