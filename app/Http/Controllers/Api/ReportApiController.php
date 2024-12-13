2d55a0f9f77b5c50128af42d227cfcb2d442a372
hanafisg196 <hanafisg196@gmail.com>
2024-12-13 23:09:25 +0700

    fix email bug

--- app/Http/Controllers/Api/ReportApiController.php
+++ app/Http/Controllers/Api/ReportApiController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReportRequest;
 use App\Http\Resources\ReportResource;
-use App\Mail\ReportWallpaper;
 use App\Models\Report;
 use App\Models\Wallpaper;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 class ReportApiController extends Controller
 {
@@ -24,15 +22,6 @@ class ReportApiController extends Controller
         $report->owner_email = $wallpaper->users->email;
         $report->owner_name = $wallpaper->users->name;
         $report->save();
-        Mail::to("anisukidev01@gmail.com")->send(new ReportWallpaper(
-            $report->wallpaper_id,
-            $report->report_token,
-            $report->reporter_email,
-            $report->decsription,
-            $report->owner_name,
-            $report->owner_email,
-            $report->wallpaper_name
-        ));
         return (new ReportResource($report))
         ->additional([
             'message' => 'Your report has been successfully sent',
